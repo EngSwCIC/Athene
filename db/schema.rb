c2ca0b6581ba35e767cef167a8fbba522d3b47ae
Joao Sousa <jpssousa97@gmail.com>
2018-05-23 17:21:33 -0300

    created video model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_17_045607) do
+ActiveRecord::Schema.define(version: 2018_05_23_201754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2018_05_17_045607) do
     t.string "nick", null: false
     t.string "senha", null: false
     t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "videos", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "video_file"
+    t.string "file_path"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
