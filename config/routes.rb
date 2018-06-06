04a8ba07baa5326880504808e9512be32c9eaac3
Dellamora <vitormd96@gmail.com>
2018-06-06 19:01:36 +0000

    Página Index criada com título, registrar e logar e lista de vídeos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   resources :videos
   resources :users
   resources :comments
@@ -9,4 +10,5 @@ Rails.application.routes.draw do
 
   match ':controller(/:action(/:id))', :via => :get #faz um match da controladora com uma action e uma id
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
 end
