2445887b061bb6feb762edabf86b99251610f8b9
RenanZX <renangodoimedeiros@gmail.com>
2018-06-06 02:01:14 +0100

    melhorias no feedback de respostas do comentário

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,5 +1,16 @@
 module VideosHelper
-	def failed_message message
-		return message == "É preciso estar logado para comentar"
-	end
+  def failed_message message
+    return message == "É preciso estar logado para comentar"
+  end
+
+  def comment_user? id
+    if cookies[:login].nil?
+      return false
+    end
+    user = User.find_by nick: cookies[:login]
+    if user.nil? 
+      return false
+    end
+    return user.id == id
+  end
 end
